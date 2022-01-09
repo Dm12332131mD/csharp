63f4accbdaa2229ac29b56b849fd731ee971d71c
DmmD <dm12332131mdgaming@gmail.com>
2022-01-08 22:24:57 -0500

    Saving

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> grades = new List<int> { 100, 90, 60, 40 };
+            List<int> gradesCopy = new List<int> { 100, 90, 60, 40 };
+            Console.WriteLine(grades.Count);
+            Console.WriteLine(grades.SequenceEqual(gradesCopy));
+        }
+    }
+}
